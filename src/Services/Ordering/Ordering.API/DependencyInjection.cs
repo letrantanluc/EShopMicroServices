1c0e8a64d7c5500a10081fec4a3908ba99a773fb
Le Luc <Letrantanluc2002@gmail.com>
2024-06-10 15:19:29 +0700

    Ordering Microservices with DDD, CQRS and Clean Architecture

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Reflection;
+
+namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        //services.AddCarter();
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.MapCarter();
+
+        return app;
+    }
+}
