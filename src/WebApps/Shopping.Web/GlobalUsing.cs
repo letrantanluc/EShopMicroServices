d823f11c766cb5aedbe6eae3f678f36733dd63af
Le Luc <Letrantanluc2002@gmail.com>
2024-07-01 19:17:58 +0700

    Building Shopping Web Client Application with Refit HttpClientFactory Library

---
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Services;
+global using Shopping.Web.Models.Basket;
